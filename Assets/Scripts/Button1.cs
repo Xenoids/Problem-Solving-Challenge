fda9c766032c4f7670a5bbd1111e86cb03f59da5
Xenoids <nicholas.tanoto@binus.ac.id>
2021-12-12 14:53:31 +0700

    initial commit

---
+++ Assets/Scripts/Button1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button1 : MonoBehaviour
+{
+    public GameObject Circle;
+    int count = 0;
+    public void setCircle(GameObject circle)
+    {
+        Instantiate(circle);
+    }
+
+    public void OnClick()
+    {
+        if(count == 0)
+        {
+        setCircle(Circle);
+        count++;
+        }
+        else Debug.Log("already input circle");
+        
+    }
+}
