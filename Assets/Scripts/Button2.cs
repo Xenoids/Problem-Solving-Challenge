07864162f24ed829e87c5ab8a78348a3562e0437
Xenoids <nicholas.tanoto@binus.ac.id>
2021-12-12 15:36:43 +0700

    initial commit

---
+++ Assets/Scripts/Button2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Button2 : MonoBehaviour
+{
+    GameObject Circle;
+    public void setrigidbody(GameObject circle)
+    {
+        circle = GameObject.FindGameObjectWithTag("Player");
+        circle.AddComponent<Rigidbody2D>();
+        Debug.Log("Sudah Jatuh");
+    }   
+
+    public void Onclick2()
+    {
+        setrigidbody(Circle);
+    }
+}
